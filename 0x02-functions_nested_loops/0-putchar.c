8041c5c00417ac86b89200885275c158e93d2e77
Nii-Armah <stanleyniiarmah7@gmail.com>
2023-06-20 19:44:20 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
-/*Write the string "_putchar" followed by a newline to stdout.*/
+/**
+ * Write the string "_putchar" followed by a newline to stdout.
+*/
 int main(void)
 {
 	write(1, "_putchar\n", 9);
